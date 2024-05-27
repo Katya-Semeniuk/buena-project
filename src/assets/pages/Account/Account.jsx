259fe44fc8cfff87f9471a13945f928d67482481
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-05-28 00:36:23 +0200

    add accout page

--- src/assets/pages/Account/Account.jsx
+++ src/assets/pages/Account/Account.jsx
@@ -1,23 +1,35 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import FormContext from "../../components/formcontext/form.context";
+
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./Account.css";
 
 function Account() {
-  const { firstName, lastName, salary } = useContext(FormContext);
+  const { firstName, salary } = useContext(FormContext);
+
+  const dataUser = {
+    fname: firstName,
+    salary: salary,
+  };
 
   useEffect(() => {
-    toast.success(`Contact ${firstName} was added`);
-  }, [firstName]);
+    if (dataUser.fname === "") {
+      toast.warn("You need to register");
+    } else {
+      toast.success(`Contact ${dataUser.fname} was added`);
+    }
+  }, [dataUser.fname]);
 
   return (
-    <div className="wrap">
-      <p>Account Page</p>
-      <div>Hello</div>
-      <p>{firstName}</p>
-      <p>{lastName}</p>
-      <p>{salary}</p>
+    <div className="wrap-container">
+      <h1 className="text-center pt-5 mb-4">
+        Hello <span className="text-primary">{dataUser.fname}</span>
+      </h1>
+      <p className=" text-center">
+        Now you can find your perfect apartment that fits your budget{" "}
+        <span className="text-primary"> {dataUser.salary}</span>
+      </p>
     </div>
   );
 }
