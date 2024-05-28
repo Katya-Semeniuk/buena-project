fba27bab59eb62cc31d5bcacb743af392b324035
Katya-Semeniuk <katyasemeniuk@gmail.com>
2024-05-28 21:31:50 +0200

    change Acccount page stylization

--- src/assets/pages/Account/Account.jsx
+++ src/assets/pages/Account/Account.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import FormContext from "../../components/formcontext/form.context";
+import Card from "react-bootstrap/Card";
 
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -22,14 +23,18 @@ function Account() {
   }, [dataUser.fname]);
 
   return (
-    <div className="wrap-container">
-      <h1 className="text-center pt-5 mb-4">
-        Hello <span className="text-primary">{dataUser.fname}</span>
-      </h1>
-      <p className=" text-center">
-        Now you can find your perfect apartment that fits your budget{" "}
-        <span className="text-primary"> {dataUser.salary}</span>
-      </p>
+    <div className="account-container d-flex justify-content-center align-items-center text-center">
+      <Card style={{ width: "35rem" }}>
+        <Card.Body>
+          <Card.Title>
+            Hello <span className="text-primary">{dataUser.fname}</span>
+          </Card.Title>
+          <Card.Text>
+            Now you can find your perfect apartment that fits your budget{" "}
+            <span className="text-primary"> {dataUser.salary}</span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
